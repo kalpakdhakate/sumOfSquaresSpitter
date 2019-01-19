3512d137f7822f04b6d93ac66116dd5ca1ca762e
Kalpak Dhakate <kalpakd@kals-air.lan>
2019-01-18 19:54:19 -0800

    Made basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputboxComponent } from './inputbox/inputbox.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputboxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/inputbox/inputbox.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inputbox',
+  templateUrl: './inputbox.component.html',
+  styleUrls: ['./inputbox.component.css']
+})
+export class InputboxComponent implements OnInit {
+
+  nums = {input: ''};
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
